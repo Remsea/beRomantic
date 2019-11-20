e6836af8c1c6e7f526f45f4d20dd8d7b20da5f02
Eva Guillen <eva.guillen6@gmail.com>
2019-11-20 12:59:01 +0100

    fix update and display photo on partenaire index

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,4 +3,17 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :thumnail do
+    resize_to_fit 256, 256
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 50,
+      width: 75, height: 75, crop: :thumb, gravity: :face
+  end
 end
