357f92dc4b2855aa8ba42b8fde917e9e8f8a91f1
Rems <remi2202@zohomail.eu>
2019-11-20 15:39:49 +0100

    adding the date to the partenaire index

--- app/controllers/key_dates_controller.rb
+++ app/controllers/key_dates_controller.rb
@@ -1,6 +1,18 @@
 class KeyDatesController < ApplicationController
-
   def create
+    @newdate = KeyDate.new(param_data)
+    @newdate.user = current_user
+    if @newdate.save
+      respond_to do |format|
+        format.html { redirect_to partenaires_path }
+        format.js # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to partenaires_path }
+        format.js # <-- idem
+      end
+    end
   end
 
   def udpate
@@ -9,5 +21,8 @@ class KeyDatesController < ApplicationController
   def destroy
   end
 
-
+  private
+  def param_data
+    params.permit(:date, :description)
+  end
 end
